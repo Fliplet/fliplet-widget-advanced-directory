07250ec210515fce716bed95ae07ed41fede119a
hcarneiro <hcarneiro@weboo.biz>
2018-01-25 10:59:00 +0000

    Fixes issue 2074

--- js/interface.templates.js
+++ js/interface.templates.js
@@ -37,9 +37,9 @@ this["Fliplet"]["Widget"]["Templates"]["interface.dataFieldSelect"] = Handlebars
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "\" id=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\n    <option value=\"\">&mdash; Select a field &mdash;</option>\n"
+    + "\">\n    <option value=\"\">-- Select a field</option>\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.fields : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "</select>\n";
+    + "</select>";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["interface.dataFieldTokenField"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
