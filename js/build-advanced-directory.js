bd5a810e7cb2672359c67e5468c0ecc15a453fdf
Tony Wu <twu@fliplet.com>
2018-04-06 18:33:50 +0100

    Refactor

--- js/build-advanced-directory.js
+++ js/build-advanced-directory.js
@@ -14,6 +14,16 @@ function html_entity_decode(html) {
   return txt.value;
 }
 
+function splitByCommas(str) {
+  if (typeof str !== 'string') {
+    return [];
+  }
+
+  // Split a string by commas but ignore commas within double-quotes using Javascript
+  // https://stackoverflow.com/questions/11456850/split-a-string-by-commas-but-ignore-commas-within-double-quotes-using-javascript
+  return str.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+}
+
 var AdvancedDirectory = function (config, container) {
   var _this = this;
 
@@ -196,9 +206,7 @@ AdvancedDirectory.prototype.initialiseHandlebars = function(){
       return '';
     }
 
-    // Split a string by commas but ignore commas within double-quotes using Javascript
-    // https://stackoverflow.com/questions/11456850/split-a-string-by-commas-but-ignore-commas-within-double-quotes-using-javascript
-    var splitTags = tags.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+    var splitTags = splitByCommas(tags);
     return new Handlebars.SafeString(
       splitTags.map(function (tag) {
         tag = tag.trim();
@@ -427,9 +435,7 @@ AdvancedDirectory.prototype.renderFilterValues = function( filter, inOverlay ){
   if (tags_field === filter) {
     this.data.forEach(function (record) {
       if (record[tags_field]) {
-        // Split a string by commas but ignore commas within double-quotes using Javascript
-        // https://stackoverflow.com/questions/11456850/split-a-string-by-commas-but-ignore-commas-within-double-quotes-using-javascript
-        var entryTags = record[tags_field].match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+        var entryTags = splitByCommas(record[tags_field]);
         entryTags.forEach(function(tag) {
           tag = tag.trim();
           if (tag !== '' && values.indexOf(tag) === -1) {
@@ -901,9 +907,7 @@ AdvancedDirectory.prototype.getEntryField = function( entryIndex, fieldIndex, ty
   if ( (typeof value === 'object' && value && value.value && value.value.length) || (typeof value === 'string' && value.length) ) {
     if (this.config.show_tags && this.config.tags_field === label && fieldType === 'filter') {
 
-      // Split a string by commas but ignore commas within double-quotes using Javascript
-      // https://stackoverflow.com/questions/11456850/split-a-string-by-commas-but-ignore-commas-within-double-quotes-using-javascript
-      var splitTags = value.value.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+      var splitTags = splitByCommas(value.value);
       valueHTML = splitTags.map(function (tag) {
         tag = tag.trim();
         if (tag !== '') {
@@ -988,9 +992,7 @@ AdvancedDirectory.prototype.renderSearchResult = function( options, callback ){
     case 'filter-value-tag':
       var filterByTag = function(value) {
         if (value[options.field]) {
-          // Split a string by commas but ignore commas within double-quotes using Javascript
-          // https://stackoverflow.com/questions/11456850/split-a-string-by-commas-but-ignore-commas-within-double-quotes-using-javascript
-          var splitTags = value[options.field].match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+          var splitTags = splitByCommas(value[options.field]);
           for (var i = 0; i < splitTags.length; i++) {
             if (splitTags[i].trim() === options.value.trim()) {
               return true;
